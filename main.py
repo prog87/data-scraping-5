8b8d467082c9bb245803aef1549da710e1db28d6
Rizky Rahman <rizkpython87@gmail.com>
2023-06-02 02:10:08 +0800

    Not Fixed

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import bs4
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://id.carousell.com/u/carousell_id/'
+html_doc = requests.get('https://id.carousell.com/categories/6/?')
+soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-contents = requests.get(url)
-# print(contents.text)
+kamera_terpopuler = soup.find(attrs={'class':'D_aH asm-browse-listings'})
+# print(kamera_terpopuler)
 
-response = bs4.BeautifulSoup(contents.text, 'html.parser')
-# print(response)
-data = response.find_all('div', 'D_lN D_ut M_mt D_uv M_mv')
-print(data[0].text)
+titles = kamera_terpopuler.findAll(attrs={'class': 'D_Xc M_No D_Xe M_Np'})
+
+for title in titles:
+    # print(title)
+    print(title.find('p',attrs={'class':'D_qE M_lC D_pQ M_lp D_qF M_lE D_qJ M_lI D_qM M_lL D_qP M_lO D_qR M_lQ D_qN M_lM D_qV'}))
+
+# Scrapping tidak selesai karena situs menggunakan beberapa konten dinamis oleh JS
+# solusi akan menggunakan selenium
 
 
 
